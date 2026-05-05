0e4fe540fd631aa6c0f4bf2afba6f9a9fe8100d6
Khalida Khan <khalidaumardaraz@gmail.com>
2026-05-05 21:28:46 +0530

    add whitenoise

--- core/settings.py
+++ core/settings.py
@@ -140,5 +140,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3',
+    conn_max_age=600
+    )
 }
